01b57ebe8f5c5b1431f97a0cd0f27ed969c6809d
sykrik <nguyentantruc89@gmail.com>
2016-07-28 17:05:46 +0700

    * Test case login.

--- src/part1/cart-summary.js
+++ src/part1/cart-summary.js
@@ -1,21 +1,54 @@
+var tax = require('./tax');
+
 function CartSummary(items) {
-    this._items = items;
-};
+	this._items = items;
+}
 
 CartSummary.prototype.getSubtotal = function () {
-    if(this._items.length){
-        return this._items.reduce(function(subtotal, item){
-            subtotal += (item.quantity * item.price);
-            return subtotal;
-        }, 0);
-    }
-    return 0;
+	if (this._items.length) {
+		return this._items.reduce(function (subtotal, item) {
+			return subtotal += (item.quantity * item.price);
+		}, 0);
+	}
+
+	return 0;
 };
 
-CartSummary.prototype.getTax = function(){
-    tax.calculate(this.getSubtotal(), state, function(){
-        done(taxInfo.amount);
-    });
+CartSummary.prototype.getTax = function (state, done) {
+    if (done == null)
+        done = function () { };
+
+	tax.calculate(this.getSubtotal(), state, function (taxInfo) {
+		done(taxInfo.amount);
+	});
 };
+
+
+function fakeRequestLogin(username, pass, callback) {
+	setTimeout(function () {
+		if (username == "admin" && pass == "admin")
+			return callback(true , {"name": "admin"})
+		return callback(false)
+	}, 50)
+}
+
+CartSummary.prototype.login = function (username, pass, onDone) {
+	if (onDone == null)
+		onDone = function () { }
+
+	
+	if(username == null || pass == null)
+		return onDone(400, "Missing Username Pass")
+
+	var onRequestDone = function(loginRes, data) {
+		if(loginRes) {
+			return onDone(null, null)
+		} else
+			return onDone(409, "Wrong User Name Pass")
+
+	}
+
+	fakeRequestLogin(username, pass, onRequestDone)
+}
 
 module.exports = CartSummary;
--- tests/part1/cart-summary-test.js
+++ tests/part1/cart-summary-test.js
@@ -4,41 +4,104 @@ var CartSummary = require('./../../src/part1/cart-summary');
 var sinon = require('sinon');
 var tax = require('./../../src/part1/tax');
 
-describe('getTax()', function () {
-    beforeEach(function () {
-        sinon.stub(tax, 'calculate', function (subtotal, state, done) {
-            setTimeout(function () {
-                done({
-                    amount: 30
-                });
-            }, 0);
-        });
-    });
+describe('CartSummary', function() {
+	beforeEach(function() {
+		sinon.stub(tax, 'calculate', function(subtotal, state, done) {
+			setTimeout(function() {
+				done({
+					amount: 30
+				});
+			}, 0);
+		});
+	});
 
-    afterEach(function () {
-        tax.calculate.restore();
-    });
+	afterEach(function() {
+		tax.calculate.restore();
+	});
 
-    it('getTax() should execute the callback function with the tax amount', function (done) {
-        var cartSummary = new CartSummary([{
-            id: 1,
-            quantity: 4,
-            price: 50
-        },{
-            id: 2,
-            quantity: 2,
-            price: 30
-        },{
-            id: 3,
-            quantity: 1,
-            price: 40
-        }]);
+	it('getSubtotal() should return 0 if no items are passed in', function() {
+		var cartSummary = new CartSummary([]);
+		expect(cartSummary.getSubtotal()).to.equal(0);
+	});
 
-        CartSummary.getTax('NY', function (taxAmount) {
-            expect(taxAmount).to.equal(30);
-            expect(tax.calculate.getCall(0).args[0]).to.equal(300);
-            expect(tax.calculate.getCall(0).args[1]).to.equal('NY');
-            done();
-        });
-    });
+	it('getSubtotal() should return the sum of the price * quantity for all items', function() {
+		var cartSummary = new CartSummary([
+			{
+				id: 1,
+				quantity: 4,
+				price: 50
+			},
+			{
+				id: 2,
+				quantity: 2,
+				price: 30
+			},
+			{
+				id: 3,
+				quantity: 1,
+				price: 40
+			}
+		]);
+		expect(cartSummary.getSubtotal()).to.equal(300);
+	});
+
+	it('getTax() should execute the callback function with the tax amount', function(done) {
+		var cartSummary = new CartSummary([
+			{
+				id: 1,
+				quantity: 4,
+				price: 50
+			},
+			{
+				id: 2,
+				quantity: 2,
+				price: 30
+			},
+			{
+				id: 3,
+				quantity: 1,
+				price: 40
+			}
+		]);
+
+		cartSummary.getTax('NY', function(taxAmount) {
+			expect(taxAmount).to.equal(30);
+			expect(tax.calculate.getCall(0).args[0]).to.equal(300);
+			expect(tax.calculate.getCall(0).args[1]).to.equal('NY');
+			done();
+		});
+	});
+
+	it('fake Login Success', function(done) {
+		var cartSummary = new CartSummary([]);
+
+		var onDone = function (err, message) {
+			expect(err).to.equal(null);
+			done()
+		}
+		
+		cartSummary.login('admin', "admin", onDone);
+	});
+
+	it('fake Login Wrong UserName Pass', function(done) {
+		var cartSummary = new CartSummary([]);
+
+		var onDone = function (err, message) {
+			expect(err).to.equal(409);
+			done()
+		}
+
+		cartSummary.login('admin', "admin1", onDone);
+	});
+
+	it('fake Login Missing UserName Pass', function(done) {
+		var cartSummary = new CartSummary([]);
+
+		var onDone = function (err, message) {
+			expect(err).to.equal(400);
+			done()
+		}
+
+		cartSummary.login(null, "admin1", onDone);
+	});
 });
